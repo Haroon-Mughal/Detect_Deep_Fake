fa4f0d5db1b8358a373d9720b6f85087e433804b
Haroon Mughal <mhwahab@bradford.ac.uk>
2025-01-14 17:22:06 +0000

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,156 @@
-#Enter Preprocessing code here
+import os
+import sys
+import time
+import cv2
+import dlib
+import yaml
+import logging
+import datetime
+import glob
+import numpy as np
+from tqdm import tqdm
+from pathlib import Path
+from imutils import face_utils
+from skimage import transform as trans
+
+def get_keypts(image, face, predictor, face_detector):
+    # detect the facial landmarks for the selected face
+    shape = predictor(image, face)
+
+    # select the key points for the eyes, nose, and mouth
+    leye = np.array([shape.part(37).x, shape.part(37).y]).reshape(-1, 2)
+    reye = np.array([shape.part(44).x, shape.part(44).y]).reshape(-1, 2)
+    nose = np.array([shape.part(30).x, shape.part(30).y]).reshape(-1, 2)
+    lmouth = np.array([shape.part(49).x, shape.part(49).y]).reshape(-1, 2)
+    rmouth = np.array([shape.part(55).x, shape.part(55).y]).reshape(-1, 2)
+
+    pts = np.concatenate([leye, reye, nose, lmouth, rmouth], axis=0)
+
+    return pts
+
+
+def extract_aligned_face_dlib(face_detector, predictor, image, res=256, mask=None):
+    def img_align_crop(img, landmark=None, outsize=None, scale=1.3, mask=None):
+        """
+        align and crop the face according to the given bbox and landmarks
+        landmark: 5 key points
+        """
+
+        M = None
+        target_size = [112, 112]
+        dst = np.array([
+            [30.2946, 51.6963],
+            [65.5318, 51.5014],
+            [48.0252, 71.7366],
+            [33.5493, 92.3655],
+            [62.7299, 92.2041]], dtype=np.float32)
+
+        if target_size[1] == 112:
+            dst[:, 0] += 8.0
+
+        dst[:, 0] = dst[:, 0] * outsize[0] / target_size[0]
+        dst[:, 1] = dst[:, 1] * outsize[1] / target_size[1]
+
+        target_size = outsize
+
+        margin_rate = scale - 1
+        x_margin = target_size[0] * margin_rate / 2.
+        y_margin = target_size[1] * margin_rate / 2.
+
+        # move
+        dst[:, 0] += x_margin
+        dst[:, 1] += y_margin
+
+        # resize
+        dst[:, 0] *= target_size[0] / (target_size[0] + 2 * x_margin)
+        dst[:, 1] *= target_size[1] / (target_size[1] + 2 * y_margin)
+
+        src = landmark.astype(np.float32)
+
+        # use skimage tranformation
+        tform = trans.SimilarityTransform()
+        tform.estimate(src, dst)
+        M = tform.params[0:2, :]
+
+        # M: use opencv
+        # M = cv2.getAffineTransform(src[[0,1,2],:],dst[[0,1,2],:])
+
+        img = cv2.warpAffine(img, M, (target_size[1], target_size[0]))
+
+        if outsize is not None:
+            img = cv2.resize(img, (outsize[1], outsize[0]))
+
+        if mask is not None:
+            mask = cv2.warpAffine(mask, M, (target_size[1], target_size[0]))
+            mask = cv2.resize(mask, (outsize[1], outsize[0]))
+            return img, mask
+        else:
+            return img, None
+
+    # Image size
+    height, width = image.shape[:2]
+
+    # Convert to rgb
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Detect with dlib
+    faces = face_detector(rgb, 1)
+    if len(faces):
+        # For now only take the biggest face
+        face = max(faces, key=lambda rect: rect.width() * rect.height())
+
+        # Get the landmarks/parts for the face in box d only with the five key points
+        landmarks = get_keypts(rgb, face, predictor, face_detector)
+
+        # Align and crop the face
+        cropped_face, mask_face = img_align_crop(rgb, landmarks, outsize=(res, res), mask=mask)
+        cropped_face = cv2.cvtColor(cropped_face, cv2.COLOR_RGB2BGR)
+
+        # Extract the all landmarks from the aligned face
+        face_align = face_detector(cropped_face, 1)
+        if len(face_align) == 0:
+            return None, None, None
+        landmark = predictor(cropped_face, face_align[0])
+        landmark = face_utils.shape_to_np(landmark)
+
+        return cropped_face, landmark, mask_face
+
+    else:
+        return None, None, None
+
+
+def draw_landmarks(image, landmarks):
+    """
+    Draw landmarks on the face image.
+    
+    Parameters:
+    - image: np.ndarray, the image array.
+    - landmarks: list of tuples, each tuple contains (x, y) coordinates of a landmark.
+    
+    Returns:
+    - Annotated image with landmarks drawn.
+    """
+    # Ensure the image is copied to avoid modifying the original
+    annotated_image = image.copy()
+
+    # Draw landmarks
+    for (x, y) in landmarks:
+        # Draw a small green 'x' at each landmark
+        cv2.drawMarker(annotated_image, (int(x), int(y)), color=(0, 255, 0), markerType=cv2.MARKER_TILTED_CROSS, thickness=1, markerSize=5)
+
+    return annotated_image
+
+
+def preprocess_img(image):
+  #define face detector and predictor models
+  face_detector = dlib.get_frontal_face_detector()
+  predictor_path = '/DeepfakeBench/preprocessing/dlib_tools/shape_predictor_81_face_landmarks.dat'
+  ## Check if predictor path exists
+  if not os.path.exists(predictor_path):
+      print(f"Error: Predictor path does not exist: {predictor_path}")   ### logger or console output
+      sys.exit()
+  face_predictor = dlib.shape_predictor(predictor_path)
+  cropped_face, landmarks, masks = extract_aligned_face_dlib(face_detector, face_predictor, image)
+
+  ann_img = draw_landmarks(cropped_face, landmarks)
+  return cropped_face, landmarks, masks, ann_img
